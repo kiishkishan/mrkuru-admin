5a563a6d594e35cc6c153db4b2633f7833c8ae7f
kishanth1 <kishanth@apptimustech.com>
2025-02-06 21:09:51 +0530

    using proxy for http issue

--- mrkuru-frontend-admin/next.config.ts
+++ mrkuru-frontend-admin/next.config.ts
@@ -11,19 +11,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Apply to all routes
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "upgrade-insecure-requests",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
